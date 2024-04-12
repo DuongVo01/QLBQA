f73e7ca559321d92ec7980612136158c0e2a4b02
DuongVo01 <votrungduong1@gmail.com>
2024-04-12 22:01:02 +0700

    Add project files.

---
+++ QLBQA/Components/Navbar.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QLBQA.Data;
+
+namespace QLBQA.Component
+{
+    public class Navbar : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Navbar(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(_context.Categories.ToList());
+        }
+    }
+}
---
+++ QLBQA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QLBQA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
